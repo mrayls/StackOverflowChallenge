abd7a9e41bebb9839fbecd0b1a026dfdd4bd19e2
Matt Rayls <mwrayls@gmail.com>
2022-03-03 14:15:03 -0600

    Initial commit

---
+++ Shared/StackOverflowChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackOverflowChallengeApp.swift
+//  Shared
+//
+//  Created by Matt Rayls on 3/3/22.
+//
+
+import SwiftUI
+
+@main
+struct StackOverflowChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
